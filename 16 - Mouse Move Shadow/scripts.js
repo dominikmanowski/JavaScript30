6623a2ab71be562ac2b44d293b7022f202ca6d17
Dominik Manowski <manowski@protonmail.com>
2019-01-15 22:32:29 +0100

    Add shadow blur

--- 16 - Mouse Move Shadow/scripts.js
+++ 16 - Mouse Move Shadow/scripts.js
@@ -14,11 +14,17 @@ function shadow(e) {
   const xWalk = Math.round((x / width) * walk - walk / 2);
   const yWalk = Math.round((y / height) * walk - walk / 2);
 
+  const shadowBlur = Math.floor(
+    Math.sqrt(Math.pow(xWalk / 2, 2) + Math.pow(yWalk / 2, 2))
+  );
+
+  console.log(shadowBlur);
+
   text.style.textShadow = `
-    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
-    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
-    ${yWalk}px ${xWalk * -1}px 0 rgba(0,0,255,0.7),
-    ${yWalk * -1}px ${xWalk}px 0 rgba(0,255,0,0.7)
+    ${xWalk}px ${yWalk}px ${shadowBlur}px rgba(255,0,255,0.7),
+    ${xWalk * -1}px ${yWalk}px ${shadowBlur}px rgba(0,255,255,0.7),
+    ${yWalk}px ${xWalk * -1}px ${shadowBlur}px rgba(0,0,255,0.7),
+    ${yWalk * -1}px ${xWalk}px ${shadowBlur}px rgba(0,255,0,0.7)
   `;
 }
 
