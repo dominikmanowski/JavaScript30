630e463e7b24565f515dbaa52154aff945cfa62e
Dominik Manowski <manowski@protonmail.com>
2019-02-05 14:40:40 +0100

    Finish day 24 - Sticky Nav

---
+++ 24 - Sticky Nav/script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+function fixNav() {
+  if (topOfNav < window.scrollY) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
